13c9ef82f7a97f1a173c23b1ecf33a4a8e4239c8
kjwon15 <kjwonmail@gmail.com>
2017-01-18 07:01:46 +0900

    Make working prototype

--- catbot.py
+++ catbot.py
@@ -1,5 +1,9 @@
 import os
+import re
 
+from io import BytesIO
+
+import requests
 import tweepy
 
 
@@ -12,25 +16,64 @@ GIPHY_API_KEY = os.getenv('GIPHY_API_KEY')
 
 
 class CatBotListener(tweepy.streaming.StreamListener):
+    PATTERNS = [
+        re.compile(r'(?:야옹이|냐옹이|냥이).*필요'),
+        re.compile(r'우울해|냐짤'),
+    ]
+
+    def __init__(self, api):
+        super(CatBotListener, self).__init__()
+        self.api = api
 
     def on_status(self, status):
         if hasattr(status, 'retweeted_status'):
             return
 
-        self.process_status(status)
+        if any((pattern.search(status.text) for pattern in self.PATTERNS)):
+            self.reply_with_cat(status)
 
-    def process_status(self, status):
-        print(status.text)
+    def reply_with_cat(self, status):
+        catpic_url = get_random_catpic_url()
+        f = BytesIO(requests.get(catpic_url).content)
+        media_id = self.api.media_upload('giphy.gif', file=f).media_id
+        self.api.update_status(
+            status='@{dest.screen_name}'.format(dest=status.user),
+            in_reply_to_stauts_id=status.id,
+            media_ids=(media_id,)
+        )
+
+
+def get_random_catpic_url():
+    json_result = requests.get('http://api.giphy.com/v1/gifs/random', {
+        'api_key': GIPHY_API_KEY,
+        'tag': 'cat'
+    }).json()
+    url = json_result['data']['image_url']
+    return url
 
 
 def main():
-    catbot_listener = CatBotListener()
-
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
+    api = tweepy.API(auth)
+    catbot_listener = CatBotListener(api)
+
     stream = tweepy.Stream(auth, catbot_listener)
     stream.userstream()
+
+
+def test():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    catpic_url = get_random_catpic_url()
+    f = BytesIO(requests.get(catpic_url).content)
+    media_id = api.media_upload('giphy.gif', file=f).media_id
+    api.update_status(
+        status='',
+        media_ids=(media_id,)
+    )
 
 
 if __name__ == '__main__':
